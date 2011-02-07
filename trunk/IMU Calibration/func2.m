eb711ca4401a9ab52bbf3a0063d0f30a5b4ef0c2
grivan.ozone <grivan.ozone@2ebb3d2b-95d4-9a1d-7d03-e22124a97a41>
2011-02-07 13:16:13 +0000

    Directory Restructure

--- trunk/IMU Calibration/func2.m
+++
@@ -1,7 +0,0 @@
-function F2= func2(thet2)
-global th1;
-global y;
-global i;
-g=9.8;
-ba= [sin(thet2(1))*(g); cos(thet2(1))* sin(thet2(2))* (-g); cos(thet2(1))*cos(thet2(2))* (-g); 1];
-F2=y(:,i)-Htheta(th1)*ba;
