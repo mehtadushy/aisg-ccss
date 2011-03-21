303be68ab20156aa85a69552eca431a7d337e072
mehtadushy <mehtadushy@2ebb3d2b-95d4-9a1d-7d03-e22124a97a41>
2011-03-21 10:29:52 +0000

    blah

--- trunk/Gyro_cal/func1.m
+++ trunk/Gyro_cal/func1.m
@@ -9,6 +9,10 @@ for i= 2: length(stat)-1
   roll=dat(stat(i),10);
   pitch=dat(stat(i),9);
   for j=stat(i)+1 : stat(i+1)
+      rolli=roll;
+      pitchi=pitch;
+      roll= roll + dat(j,7)*(1/1000.0)* sec(pitchi*pi/180) * (1/(thet(2)*thet(3)))*(dat(j,4)-thet(4));
+      pitch= pitch + dat(j,7)*(1/1000.0)* sec(pitchi*pi/180) *(( (1/(thet(1)*thet(3)))*(dat(j,5)-thet(5))* cos(rolli*pi/180)) -((1/(thet(1)*thet(2)))*(dat(j,6)-thet(6))* sin(rolli*pi/180)));
       if roll> 180
           roll= roll -360;
       end
@@ -17,10 +21,7 @@ for i= 2: length(stat)-1
       end
       pitch=sin(pitch*pi/180);
       pitch= asin(pitch)*180/pi;
-      rolli=roll;
-      pitchi=pitch;
-      pitch= pitch + dat(j,7)*(1/1000.0)* sec(rolli*pi/180) * (1/(thet(2)*thet(3)))*(dat(j,4)-thet(4));
-      roll= roll + dat(j,7)*(1/1000.0)* sec(rolli*pi/180) *(( (1/(thet(1)*thet(3)))*(dat(j,5)-thet(5))* cos(pitchi*pi/180)) -((1/(thet(1)*thet(2)))*(dat(j,6)-thet(6))* sin(pitchi*pi/180)));
+  
   end
   
       
@@ -29,7 +30,7 @@ for i= 2: length(stat)-1
   %pitch= asin(pitch)*180/pi;
   rollcoll=[rollcoll; roll]
   pitchcoll=[pitchcoll; pitch]
-  f=[f; (dat(stat(i+1),9)-pitch); (dat(stat(i+1),10)-roll)];
+  f=[f; 100*(dat(stat(i+1),9)-pitch); 100*(dat(stat(i+1),10)-roll)];
   %f=[f; (dat(stat(i+1),9)-pitch)];
  %end
 end
